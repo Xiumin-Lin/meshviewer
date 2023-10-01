1df48296d91e7f535ca25bf49c5825e423e941c1
Xiumin-Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2023-10-01 14:29:58 +0200

    Update myFace.cpp

--- myproj/myFace.cpp
+++ myproj/myFace.cpp
@@ -17,5 +17,15 @@ myFace::~myFace(void)
 
 void myFace::computeNormal()
 {
-/**** TODO ****/
+	/**** TODO ****/
+	myPoint3D* p1 = this->adjacent_halfedge->source->point;
+	myPoint3D* p2 = this->adjacent_halfedge->next->source->point;
+	myPoint3D* p3 = this->adjacent_halfedge->prev->source->point;
+
+	myVector3D v1 (p2->X - p1->X, p2->Y - p1->Y, p2->Z - p1->Z);
+	myVector3D v2 (p3->X - p1->X, p3->Y - p1->Y, p3->Z - p1->Z);
+
+	if (normal) delete normal;
+
+	normal = &(v1.crossproduct(v2));
 }
